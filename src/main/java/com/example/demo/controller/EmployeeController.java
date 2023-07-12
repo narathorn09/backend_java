54d9e4a8b850269579a353a31ab81d0b0b2e2a8a
Narathorn Noophum <6310210181@psu.ac.th>
2023-07-12 12:52:48 +0700

    finish CRUD

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -5,11 +5,10 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Employee;
@@ -24,6 +23,16 @@ public class EmployeeController {
 		return data;
 	}
 	
+	@GetMapping("/employee/{employeeId}")
+	public Employee getEmployeeById(@PathVariable("employeeId") Integer employeeId) {
+		for(int i = 0; i < data.size(); i++) {
+			if(data.get(i).getEmployeeId().equals(employeeId)) {
+				return data.get(i);
+			}
+		}
+		return null;
+	}
+	
 	@PostMapping("/employee")
 	public Employee addEmployee(@RequestBody Employee body) {
 		for(int i = 0; i < data.size(); i++) {
@@ -35,10 +44,22 @@ public class EmployeeController {
 		return body;
 	}
 	
-	@DeleteMapping("/employee/:employeeId")
-	public String deleteEmployee(@RequestParam Integer employeeId) {
+	@PutMapping("/employee/{employeeId}")
+	public String updateEmployee(@PathVariable("employeeId") Integer employeeId, @RequestBody Employee body) {
 		for(int i = 0; i < data.size(); i++) {
-			if(data.get(i).getEmployeeId() == employeeId) {
+			if(data.get(i).getEmployeeId().equals(employeeId)) {
+				body.setEmployeeId(employeeId);
+	            data.set(i, body);
+				return "Update Employee Success";
+			}
+		}
+		return "Error Update Employee";
+	}
+	
+	@DeleteMapping("/employee/{employeeId}")
+	public String deleteEmployee(@PathVariable("employeeId") Integer employeeId) {
+		for(int i = 0; i < data.size(); i++) {
+			if(data.get(i).getEmployeeId().equals(employeeId)) {
 				data.remove(i);
 				return "Delete Employee Success";
 			}
